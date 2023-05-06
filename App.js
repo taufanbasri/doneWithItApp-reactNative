b73b7994a523d7e0093e8e57e7a71f6b0e3b8d3a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-06 22:45:00 +0700

    View Image Screen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
