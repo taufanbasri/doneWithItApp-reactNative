a58b9435029398c058458cb45bcc624dd28941bf
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-07 11:28:12 +0700

    ListItem Component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Image, StyleSheet, View } from 'react-native'
 import AppText from '../components/AppText'
 import colors from '../config/colors'
+import ListItem from '../components/ListItem'
 
 const ListingDetailsScreen = () => {
   return (
@@ -11,6 +12,14 @@ const ListingDetailsScreen = () => {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/profile.png')}
+            title='Taufan Prasetyo'
+            subTitle='5 Listings'
+          />
+        </View>
       </View>
     </View>
   )
@@ -33,6 +42,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '500'
+  },
+  userContainer: {
+    marginVertical: 40,
   }
 })
 
