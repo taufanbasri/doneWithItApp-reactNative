08e0a1b2fdf476d50dbb46ebddf37e3930134e93
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-07 10:36:31 +0700

    Card Component

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { View } from 'react-native';
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import Card from './app/components/Card';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <View style={{
+      backgroundColor: '#f8f4f4',
+      padding: 20,
+      paddingTop: 100
+    }}>
+      <Card
+        title='Red jacket for sale'
+        subTitle='$100'
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
   );
 }
---
+++ app/components/AppText.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Platform, StyleSheet, Text } from 'react-native'
+
+const AppText = ({ children, style }) => {
+  return (
+    <Text style={[styles.text, style]}>{children}</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir'
+  }
+})
+
+export default AppText
