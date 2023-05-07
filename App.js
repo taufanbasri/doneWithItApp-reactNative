dae65aeea0ec68940565fa57a2de7905ca05f6e6
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-07 08:56:03 +0700

    Encapsulate styles

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import { View } from 'react-native';
+import AppText from './app/components/AppText';
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <AppText>I love React Native!</AppText>
+    </View>
   );
 }
