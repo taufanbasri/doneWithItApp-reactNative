32aefd1315c4b25e42d406a3c07ec14c87e0fab5
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-07 21:52:08 +0700

    Icon Component

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-import MessagesScreen from './app/screens/MessagesScreen';
+import Icon from './app/components/Icon';
+import Screen from './app/components/Screen';
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <Icon
+        name='email'
+        size={100}
+      />
+    </Screen>
   );
 }
