c703e66cac932013a19b1ff34e878c1a761f6c31
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-08 18:45:45 +0700

    TextInput Component

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import { View } from 'react-native';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
   return (
-    <ListingsScreen />
+    <View>
+      <AppTextInput placeholder='Username' icon='email' />
+    </View>
   );
 }
---
+++ app/components/AppTextInput.js
@@ -0,0 +1,34 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Platform, StyleSheet, TextInput, View } from 'react-native';
+import colors from '../config/colors';
+
+const AppTextInput = ({ icon, ...otherProps }) => {
+  return (
+    <View style={styles.container}>
+      {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon} />}
+
+      <TextInput style={styles.textInput} {...otherProps} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: 'row',
+    width: '100%',
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  textInput: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir'
+  },
+})
+
+export default AppTextInput
