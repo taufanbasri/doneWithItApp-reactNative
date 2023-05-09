b305317c80f6b33e716b8d0605aed983f47dc75f
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-09 14:07:12 +0700

    ListingEditScreen Component

---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Text } from 'react-native'
+import * as Yup from 'yup'
+import Screen from '../components/Screen'
+import { AppForm, AppFormField, AppFormPicker, SubmitButton } from '../components/forms'
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label('Title'),
+  price: Yup.string().required().min(1).max(10000).label('Price'),
+  description: Yup.string().label('Description'),
+  category: Yup.string().required().nullable().label('Category'),
+})
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Camera', value: 3 },
+]
+
+const ListingEditScreen = () => {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{
+          title: '',
+          price: '',
+          description: '',
+          category: null,
+        }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField maxLength={255} name='title' placeholder='Title' />
+        <AppFormField keyboardType='numeric' maxLength={8} name='price' placeholder='Price' />
+        <AppFormPicker items={categories} name='category' placeholder='Category' />
+        <AppFormField maxLength={255} multiline name='description' numberOfLines={3} placeholder='Description' />
+
+        <SubmitButton title='Post' />
+      </AppForm>
+    </Screen>
+  )
+}
+
+export default ListingEditScreen
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  }
+})
