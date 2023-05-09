849b869a4d1b8d2247fc8973490198e157cd7c6a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-09 14:28:30 +0700

    Fix placeholder color

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -21,9 +21,16 @@ const AppPicker = ({ icon, items, placeholder, onSelectItem, selectedItem }) => 
             />
           )}
 
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>
+              {selectedItem.label}
+            </AppText>
+          ) : (
+            <AppText style={styles.placeholder}>
+              {placeholder}
+            </AppText>
+          )}
+
           <MaterialCommunityIcons
             name='chevron-down'
             size={20}
@@ -65,6 +72,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  placeholder: {
+    color: defaultStyles.colors.medium,
+    flex: 1,
   },
   text: {
     flex: 1,
