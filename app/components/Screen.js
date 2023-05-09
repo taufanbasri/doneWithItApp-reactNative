979aa3c4baa2e1318e3f1dfd015b77c5b70ea53b
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-09 19:20:15 +0700

    Improve messages screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -5,7 +5,7 @@ import { SafeAreaView, StyleSheet, View } from 'react-native'
 const Screen = ({ children, style }) => {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View>
+      <View style={[styles.view, style]}>
         {children}
       </View>
     </SafeAreaView>
@@ -15,6 +15,9 @@ const Screen = ({ children, style }) => {
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
     flex: 1,
   }
 })
