3b12a48a7b6f6925f00b1b9957f064e0326071d9
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-10 23:27:14 +0700

    Permissions & Access the library

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import * as ImagePicker from 'expo-image-picker';
+import { useEffect, useState } from 'react';
+import { Button, Image } from 'react-native';
+import AppButton from './app/components/Button';
+import Screen from './app/components/Screen';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState()
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (!granted) alert('You need to enable permission to access the library')
+  }
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync()
+      if (!result.canceled) setImageUri(result.uri)
+    } catch (error) {
+      console.log('Error reading an image', error)
+    }
+  }
+
   return (
-    <ListingEditScreen />
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200, }} />
+    </Screen>
   );
 }
