de0ab97aa21e7e99e28bed6f11bbfe532916c0ba
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-11 14:05:31 +0700

    ImageInputList Component

--- App.js
+++ App.js
@@ -1,37 +1,24 @@
-import * as ImagePicker from 'expo-image-picker';
-import { useEffect, useState } from 'react';
-import { Button, Image } from 'react-native';
-import AppButton from './app/components/Button';
+import { useState } from 'react';
+import ImageInputList from './app/components/ImageInputList';
 import Screen from './app/components/Screen';
-import ImageInput from './app/components/ImageInput';
 
 export default function App() {
-  const [imageUri, setImageUri] = useState()
+  const [imageUris, setImageUris] = useState([])
 
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
-
-    if (!granted) alert('You need to enable permission to access the library')
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri])
   }
 
-  useEffect(() => {
-    requestPermission()
-  }, [])
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync()
-      if (!result.canceled) setImageUri(result.assets[0].uri)
-    } catch (error) {
-      console.log('Error reading an image', error)
-    }
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri))
   }
 
   return (
     <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={uri => setImageUri(uri)}
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
       />
     </Screen>
   );
---
+++ app/components/ImageInputList.js
@@ -0,0 +1,30 @@
+import { StyleSheet, View } from 'react-native'
+import ImageInput from './ImageInput'
+
+const ImageInputList = ({ imageUris = [], onAddImage, onRemoveImage }) => {
+  return (
+    <View style={styles.container}>
+      {imageUris.map(uri => (
+        <View key={uri} style={styles.image}>
+          <ImageInput
+            imageUri={uri}
+            onChangeImage={() => onRemoveImage(uri)}
+          />
+        </View>
+      ))}
+
+      <ImageInput onChangeImage={uri => onAddImage(uri)} />
+    </View>
+  )
+}
+
+export default ImageInputList
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  image: {
+    marginRight: 10,
+  }
+})
